46055cbe5c102b93b25e730b4d26303fe2c9e551
cryptocode <cryptocode@zolo.io>
2019-03-18 10:53:50 +0100

    Nodejs sample

---
+++ index.js
@@ -0,0 +1,28 @@
+const WS = require('ws');
+const ReconnectingWebSocket = require('reconnecting-websocket');
+
+// Create a reconnecting WebSocket.
+// In this example, we wait a maximum of 2 seconds before retrying.
+const ws = new ReconnectingWebSocket('ws://localhost:7078', [], {
+	WebSocket: WS,
+	connectionTimeout: 1000,	
+	maxRetries: 100000,
+	maxReconnectionDelay: 2000
+});
+
+// As soon as we connect, subscribe to block confirmations
+ws.onopen = () => {
+	const subscription = {
+		"action": "subscribe", 
+		"topic": "confirmation"
+	}
+	ws.send(JSON.stringify(subscription));
+};
+
+// The node sent us a confirmation
+ws.onmessage = msg => {
+	console.log(msg.data);
+	if (msg.data.topic === "confirmation") {
+		console.log ('Confirmed', data.message.hash)
+	}
+};
