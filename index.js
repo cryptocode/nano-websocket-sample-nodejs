49eb0f6d30000019a4443eb84f6defa0eb8ae233
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2019-04-11 14:38:01 +0100

    Fix message handling by parsing as JSON; minimize the initial connection time

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ const ws = new ReconnectingWebSocket('ws://[::1]:7078', [], {
 	WebSocket: WS,
 	connectionTimeout: 1000,	
 	maxRetries: 100000,
-	maxReconnectionDelay: 2000
+	maxReconnectionDelay: 2000,
+	minReconnectionDelay: 10 // if not set, initial connection will take a few seconds by default
 });
 
 // As soon as we connect, subscribe to block confirmations
 ws.onopen = () => {
-	const subscription = {
+	const confirmation_subscription = {
 		"action": "subscribe", 
 		"topic": "confirmation"
 	}
-	ws.send(JSON.stringify(subscription));
+	ws.send(JSON.stringify(confirmation_subscription));
+
+	// Other subscriptions can go here
 };
 
-// The node sent us a confirmation
+// The node sent us a message
 ws.onmessage = msg => {
 	console.log(msg.data);
-	if (msg.data.topic === "confirmation") {
-		console.log ('Confirmed', data.message.hash)
+	data_json = JSON.parse(msg.data);
+
+	if (data_json.topic === "confirmation") {
+		console.log ('Confirmed', data_json.message.hash)
 	}
 };
